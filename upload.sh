b5ab84255223ee498b76c6ae6a422db564be744c
Chito Wong <chi.to.wong@outlook.com>
2023-02-05 01:33:28 +0800

    Create upload artefacts action

---
+++ upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eux
+
+# git config
+git config --global user.name 'github-actions[bot]'
+git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+
+mkdir -p ./repo/"$APP_NAME"/
+cp -r ./"$DIST"/ ./repo/"$APP_NAME"/"$VERSION"/
+
+git add .
+git commit -m "Build version $APP_NAME-$VERSION"
+{
+  git push
+} || {
+  git pull --rebase
+  git push
+}
