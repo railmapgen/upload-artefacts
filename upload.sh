45d70cc41c6b6ab4a8cd1597b5d60dbb73c83b8b
Chito Wong <chi.to.wong@outlook.com>
2023-10-15 23:55:34 +0800

    Use gh-api to upload artefacts

--- upload.sh
+++ upload.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -eux
 
-# git config
-git config --global user.name 'github-actions[bot]'
-git config --global user.email 'github-actions[bot]@users.noreply.github.com'
-
-mkdir -p ./repo/"$APP_NAME"/
 tar -czvf "$VERSION".tar.gz --directory "$DIST" .
-cp "$VERSION".tar.gz ./repo/"$APP_NAME"/
 
-cd ./repo/
-git add .
-git commit -m "Build version $APP_NAME-$VERSION"
+MESSAGE="Build version $APP_NAME-$VERSION"
+cat >payload.json <<EOF
 {
-  git push
-} || {
-  git pull --rebase
-  git push
+  "message": "$MESSAGE",
+  "committer": {
+    "name": "github-actions[bot]",
+    "email": "github-actions[bot]@users.noreply.github.com"
+  },
+  "content": "$(base64 -i $VERSION.tar.gz)"
 }
+EOF
+
+echo $TOKEN | gh auth login --with-token
+cat payload.json | gh api -X PUT /repos/uat-railmapgen/rmg-repositories/contents/$APP_NAME/$VERSION.tar.gz --input -
